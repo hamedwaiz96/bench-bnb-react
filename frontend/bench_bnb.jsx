0ed549222da1165c50667dd81add407f925ec057
Hamed Waiz <hamedw96@mgmail.com>
2019-08-09 22:06:49 -0700

    Finish Phase 1

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {login, signup, logout} from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
+    window.login = login;
+    window.signup = signup;
+    window.logout = logout;
+    const store = configureStore();
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
     const root = document.getElementById("root");
     ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root)
 });
